d4c42cc2a1a2a552981b4b2839efa8ebb2c08870
Vexcited <vexitofficial@gmail.com>
2021-02-03 14:53:13 +0100

    Removed ionic files. Added PWA to OneTwoTrie WebApp. Used CDN for FontAwesome.

---
+++ pages/assets/js/app.js
@@ -0,0 +1,5 @@
+if('serviceWorker' in navigator){
+    navigator.serviceWorker.register('/pages/assets/js/serviceWorker.js')
+      .then(reg => console.log('Service Worker registered'))
+      .catch(err => console.log('Service Worker not registered', err));
+}
